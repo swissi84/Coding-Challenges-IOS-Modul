ca87f61b648560dc86ed837c9bd087205b0c4a05
Andre Eggenschwiler <swissi1984@gmail.com>
2024-08-28 09:41:39 +0200

    Initial Commit

---
+++ Coding Challange/Coding_ChallangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coding_ChallangeApp.swift
+//  Coding Challange
+//
+//  Created by Eggenschwiler Andre on 28.08.24.
+//
+
+import SwiftUI
+
+@main
+struct Coding_ChallangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coding Challange/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Coding Challange
+//
+//  Created by Eggenschwiler Andre on 28.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
