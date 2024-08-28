ed2100367a2feab8661cbf0ea67311f21809af4b
Andre Eggenschwiler <swissi1984@gmail.com>
2024-08-28 12:03:51 +0200

    no message

--- Coding Challange/ContentView.swift
+++ Coding Challange/ContentView.swift
@@ -10,7 +10,11 @@ struct ContentView: View {
     var body: some View {
         VStack(alignment: .center) {
             Text("Books Buy")
+                .frame(width: 600, height: 80)
+                .bold()
                 .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .foregroundColor(.white)
+                .background(Color.red)
             ScrollView{
                 VStack{
                     BookView(book: booksList [0])
